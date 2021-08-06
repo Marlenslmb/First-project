5c6e207eca36a20186c56e581f939ce63b5f01b6
Talgarenos <talgar_16@mail.ru>
2021-08-06 11:43:23 +0600

    detail

---
+++ src/components/Home/Content.jsx
@@ -0,0 +1,13 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import ProductList from '../Product/ProductsList';
+
+const Content = () => {
+    return (
+        <Grid>
+            <ProductList/>
+        </Grid>
+    );
+};
+
+export default Content;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import Content from './Content';
+
+const Home = () => {
+    return (
+        <div>
+            <Grid>
+                <Content/>
+            </Grid>
+        </div>
+    );
+};
+
+export default Home;
