d5d749cc6b12b298eb3b0ed38d7cae03ef9c5597
marlenslmb <marlenslmb@gmail.com>
2021-08-11 11:30:21 +0600

    Карточки

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -49,13 +49,14 @@ export default function ProductCard({item, history}) {
     <div data-aos="flip-left"
     data-aos-easing="ease-out-cubic"
     >
-    <Card style={{ width: '20rem' }} >
-    <Card.Img variant="top" style={{width: '165px', margin: '0 auto',}} src={item.image} />
-    <Card.Body>
+    <Card style={{ width: '20rem', height: 600}} >
+    
+    <Card.Img variant="top" style={{width: '165px', margin: '0 auto', height: 200}} src={item.image} />
+    <Card.Body style={{flexGrow: 1, flexDirection: 'column'}}>
       <Card.Title>
           {item.title}
       </Card.Title>
-      <Card.Text>
+      <Card.Text style={{flexGrow: 1}}>
         {item.description.substring(0, 100)}...
       </Card.Text>
     </Card.Body>
@@ -65,8 +66,8 @@ export default function ProductCard({item, history}) {
       <ListGroupItem>Цена: {item.price}р.</ListGroupItem> 
     </ListGroup>
     <Edit open={open} handleClose={handleClose} handleOpen={handleOpen} />
-    {
-      isAdmin ? (<Card.Body>
+    {/* {
+      isAdmin ? (<Card.Body style={{flex: 1}}>
         <Card.Link  style={{color: 'black', textDecoration: 'none'}}>
         <Button onClick={()=>handleOpen()} style={{backgroundColor: 'rgba(19, 16, 16, 0.932)'}}>
           &#9997;
@@ -79,8 +80,24 @@ export default function ProductCard({item, history}) {
     </Card.Link>
     </Card.Body>
       ) : (null)
-    }
-    <Card.Body>
+    } */}
+    <Card.Body style={{flex: 1, flexDirection: 'column'}}>
+      {
+        isAdmin && (
+          <div>
+            <Card.Link  style={{color: 'black', textDecoration: 'none'}}>
+              <Button onClick={()=>handleOpen()} style={{backgroundColor: 'rgba(19, 16, 16, 0.932)'}}>
+                &#9997;
+              </Button>
+            </Card.Link>
+            <Card.Link >
+            <Button aria-label="share" onClick={() => deleteProduct(item.id, history)} style={{backgroundColor: 'rgba(19, 16, 16, 0.932)'}}>
+              &#10060;
+            </Button>
+          </Card.Link>  
+         </div>
+        )
+      }
       <Card.Link style={{color: 'black', textDecoration: 'none'}}>
         <Link to={`/detail/${item.id}`}>
           <Button style={{backgroundColor: 'rgba(19, 16, 16, 0.932)'}}>
