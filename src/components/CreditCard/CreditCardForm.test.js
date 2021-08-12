88cfcb9ceb6e5623ef78f4cd1706a1166c3ce7ea
Talgarenos <talgar_16@mail.ru>
2021-08-12 15:24:34 +0600

    payment

--- src/components/CreditCard/CreditCardForm.test.js
+++ src/components/CreditCard/CreditCardForm.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import CreditCardForm from "./CreditCardForm";
+import CreditCardForm from "./CreditCardForm.test";
 import validateInfo from "./../validateInfo";
 
 const valid_values = {
